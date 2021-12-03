9e52da33618bdc9df28b22e4ca76b3c92c6d2540
April Granzow <apriljgranzow@gmail.com>
2021-12-03 10:54:27 -0800

    Added middleware for encoding

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello World');
 });
 
 app.listen(port, () => {
-  console.log()
-})
+  console.log(`Tarot Tool App listening at http://localhost:${port}`);
+});
