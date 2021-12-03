83531da08737d5e030161cf466d3ef38e5f4f426
April Granzow <apriljgranzow@gmail.com>
2021-12-03 15:35:50 -0800

    Set up webpack and React is working

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '/../dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
 const tarotApiUrl = 'https://rws-cards-api.herokuapp.com/api/v1/cards';
 
