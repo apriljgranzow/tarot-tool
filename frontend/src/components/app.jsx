2a42c6922adb54362b08fd3cf0d07f07dce19f9b
April Granzow <apriljgranzow@gmail.com>
2021-12-04 14:02:23 -0800

    Added signup and signin forms + buttons to page

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Spread from './Spread';
 import Results from './Results';
 import Signup from './Signup';
+import SignIn from './Signin';
 
 const themes = {
   'Past Present Future': ['Past', 'Present', 'Future'],
@@ -12,8 +13,10 @@ const App = function () {
   const [isLoaded, setLoaded] = useState(false);
   const [cards, setCards] = useState([]);
   const [spread, setSpread] = useState('Past Present Future');
-  const [showForm, setShowForm] = useState(false);
-  const [n, setN] = useState(2);
+  const [showSignup, setShowSignup] = useState(false);
+  const [showSignin, setShowSignin] = useState(false);
+  const [showSubmit, setShowSubmit] = useState(false);
+  const [n, setN] = useState(3);
   const dealCards = () => {
     axios({
       method: 'get',
@@ -23,11 +26,14 @@ const App = function () {
 
   return (
     <div>
-      <button type="button" id="sign-in" onClick={() => { setShowForm(true); }}>Sign in</button>
+      <button type="button" id="signup" onClick={() => { setShowSignup(true); }}>Sign Up</button>
+      <button type="button" id="signup" onClick={() => { setShowSignin(true); }}>Sign In</button>
       <h1>Tarot Tool</h1>
-      {showForm === true ? <Signup /> : ''}
+      {showSignup === true ? <Signup /> : ''}
+      {showSignin === true ? <Signup /> : ''}
       <section id="button-container">
         <button type="button" onClick={dealCards}>Tell my fortune</button>
+        <button type="button" onClick={() => {}}>Save my fortune</button>
       </section>
       {isLoaded === true ? <Spread cards={cards} /> : <Spread cards={Array(n).fill('')} />}
       {isLoaded === true ? <Results cards={cards} themes={themes[spread]} /> : ''}
