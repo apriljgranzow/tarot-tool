7cdcbc493a65e391ce00d1bed32c6cc2ee55abee
April Granzow <apriljgranzow@gmail.com>
2021-12-04 13:59:01 -0800

    Signup form with firebase user creation

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const Signup = function () {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const erorrCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
     <fieldset>
       <label>
         Email:
-        <input name="email" type="email" required />
+        <input
+          name="email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
       </label>
       <label>
-        Password
-        <input type="password" name="password" minLength="8" id="password" required />
+        Password:
+        <input
+          type="password"
+          name="password"
+          minLength="8"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
       </label>
       </fieldset>
       <input type="submit" value="Register" />
