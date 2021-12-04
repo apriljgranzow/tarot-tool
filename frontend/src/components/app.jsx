600373488c8f7ee26867a853f853b0ef91e903b9
April Granzow <apriljgranzow@gmail.com>
2021-12-04 13:27:30 -0800

    Add signup form toggle to main page

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Spread from './Spread';
 import Results from './Results';
-import Card from './Card';
+import Signup from './Signup';
 
 const themes = {
   'Past Present Future': ['Past', 'Present', 'Future'],
@@ -12,6 +12,7 @@ const App = function () {
   const [isLoaded, setLoaded] = useState(false);
   const [cards, setCards] = useState([]);
   const [spread, setSpread] = useState('Past Present Future');
+  const [showForm, setShowForm] = useState(false);
   const [n, setN] = useState(2);
   const dealCards = () => {
     axios({
@@ -22,8 +23,12 @@ const App = function () {
 
   return (
     <div>
+      <button type="button" id="sign-in" onClick={() => { setShowForm(true); }}>Sign in</button>
       <h1>Tarot Tool</h1>
-      <button type="button" onClick={dealCards}>Tell my fortune</button>
+      {showForm === true ? <Signup /> : ''}
+      <section id="button-container">
+        <button type="button" onClick={dealCards}>Tell my fortune</button>
+      </section>
       {isLoaded === true ? <Spread cards={cards} /> : <Spread cards={Array(n).fill('')} />}
       {isLoaded === true ? <Results cards={cards} themes={themes[spread]} /> : ''}
     </div>
